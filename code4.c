f608389975cc24e325eb7c5f4b42d3d017914c9b
AYUSH SINGH <ayushsingh1610@outlook.com>
2023-05-25 12:19:25 +0530

    new

--- code4.c
+++ code4.c
@@ -1,2 +1,33 @@
 // Re-allocate the memory using realloc() function
 // void *realloc( void *ptr, size_t new_size );
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int *ptr;
+    int n;
+    printf("Enter the num - ");
+    scanf("%d",&n);
+    ptr = calloc(n, sizeof(int));
+
+    if(ptr==NULL)
+    printf("Memory Not Allocated");
+
+    else
+    {
+        printf("Memory allocated successfully\n");
+        for(int i=0;i<n;i++)
+        {
+            printf("ptr[%d] = %d\n",i,i+1);
+        }
+        printf("Enter the NEW SIZE of array - ");
+        scanf("%d",&n);
+        // Reallocate memory
+        ptr = realloc(ptr, n*sizeof(int));
+        printf("After reallocation\n");
+        for(int i=0;i<n;i++)
+        {
+            printf("ptr[%d] = %d\n",i,i+1);
+        }
+    }
+}
